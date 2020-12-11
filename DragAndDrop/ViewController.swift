1f8a589f56b89bc285a1161456e5a6d186852500
David Svensson <davidsvson@gmail.com>
2020-12-11 12:03:49 +0100

    Dragging dog and cat

--- DragAndDrop/ViewController.swift
+++ DragAndDrop/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func catTapped(_ sender: UITapGestureRecognizer) {
+        
+        print("Cat is tapped")
+        
+    }
+    
+    @IBAction func hanldePan(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: self.view)
+        
+        
+        if let viewToPan = sender.view {
+            viewToPan.center = CGPoint(x: viewToPan.center.x + translation.x,
+                                       y: viewToPan.center.y + translation.y)
+        }
+        
+        sender.setTranslation(CGPoint.zero, in: self.view)
+    
+    }
 }
 
